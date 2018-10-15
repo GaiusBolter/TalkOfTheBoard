139af887b0e837b16707d3e1e2c38737a3a6b34c
Gaius <gaiusbolter@gmail.com>
2018-10-15 12:19:08 +0200

    Added scraping functions

---
+++ core/scraper.py
@@ -0,0 +1,29 @@
+from urllib.request import urlopen, urlretrieve, Request
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+import os
+def validate():
+
+    url = input("Enter the URL: ")
+    validurl = urlparse(url)
+
+    while validurl.scheme != 'http' and validurl.scheme != "https":
+        url = input("Invalid URL. Enter correct URL: ")
+        validurl = urlparse(url)
+
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'}) #User agent required to avoid 403print("Incorrect URL.")
+    html = urlopen(req)
+    soup = BeautifulSoup(html, features="html.parser")
+
+    return soup
+
+def download():
+    picsoup = validate()
+    pics = picsoup.findAll("a", {"class": "fileThumb"})
+
+    for pic in pics:
+        filename=pic['href'].split("/")[-1]
+        print("Downloading " + filename)
+        urlretrieve("http:" + pic['href'], os.path.join(os.getcwd(), filename))
+
+download()
